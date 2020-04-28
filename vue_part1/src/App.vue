4db774ee356920949ec8b4f7acb745bdb1ff1a33
jonsoku2 <whdtjr2792@gmail.com>
2020-04-28 10:12:40 +0900

    [vue] 4월 27일 주중스터디 6차 커밋

--- vue_part1/src/App.vue
+++ vue_part1/src/App.vue
@@ -1,18 +1,15 @@
 <template>
-  <div id="app">
-    <Todo />
-  </div>
+  <div id="app"><Todo />asd</div>
 </template>
 
 <script>
-import Todo from "./components/Todo";
-
+import Todo from "./components/Todo"
 export default {
   name: "App",
   components: {
     Todo,
   },
-};
+}
 </script>
 
 <style></style>
