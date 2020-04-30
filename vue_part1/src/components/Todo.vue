fc05005fef4746ff66a798c15f3f672f5971a879
jonsoku2 <whdtjr2792@gmail.com>
2020-04-30 15:11:00 +0900

    [vue_part1] filter 추가

--- vue_part1/src/components/Todo.vue
+++ vue_part1/src/components/Todo.vue
@@ -1,8 +1,19 @@
 <template>
   <div class="container todo">
+    <div class="todo__filters">
+      <button @click="changeFilter('all')" class="todo__filter" :class="{ 'todo__filter--active': filter === 'all' }">
+        All ({{ allConut }})
+      </button>
+      <button @click="changeFilter('todo')" class="todo__filter" :class="{ 'todo__filter--active': filter === 'todo' }">
+        Todo ({{ todoCount }})
+      </button>
+      <button @click="changeFilter('done')" class="todo__filter" :class="{ 'todo__filter--active': filter === 'done' }">
+        Done ({{ doneCount }})
+      </button>
+    </div>
     <todo-input />
     <div class="todo__items">
-      <todo-item v-for="todo in todos" :key="todo.id" :todo="todo" />
+      <todo-item v-for="todo in filterTodo" :key="todo.id" :todo="todo" />
     </div>
   </div>
 </template>
@@ -21,15 +32,38 @@ export default {
   data() {
     return {
       content: '',
+      filter: 'all',
     };
   },
   computed: {
-    todos() {
-      return this.$store.getters.getTodos;
+    filterTodo() {
+      const todos = this.$store.getters.getTodos;
+      switch (this.filter) {
+        case 'all':
+          return todos;
+        case 'todo':
+          return todos.filter(todo => !todo.completed);
+        case 'done':
+          return todos.filter(todo => todo.completed);
+        default:
+          return todos;
+      }
+    },
+    allConut() {
+      return this.$store.getters.getTodos.length;
+    },
+    todoCount() {
+      return this.$store.getters.getTodos.filter(todo => !todo.completed).length;
+    },
+    doneCount() {
+      return this.allConut - this.todoCount;
     },
   },
   methods: {
     ...mapActions(['bindTodosRef']),
+    changeFilter(filter) {
+      this.filter = filter;
+    },
   },
   created() {
     this.bindTodosRef();
