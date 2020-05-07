6a94699a76166ed3b64029a5c2c96d9d1d39c61c
jonsoku2 <whdtjr2792@gmail.com>
2020-05-07 22:50:54 +0900

    [markup_part3] 완성

--- markup_part3/src/scripts/index.js
+++ markup_part3/src/scripts/index.js
@@ -1,3 +1,28 @@
+import Swiper from 'swiper';
 import '../styles/index.scss';
+import 'swiper/css/swiper.min.css';
 
-console.log('webpack starterkit');
+var cboxSlider = new Swiper('.swiper-container.cbox1__slider-main', {
+  direction: 'vertical',
+  slidesPerView: 1,
+  spaceBetween: 30,
+  mousewheel: true,
+  pagination: {
+    el: '.swiper-pagination.cbox1__slider-pagination',
+    clickable: true,
+  },
+});
+
+const mainSwiper = new Swiper('.swiper-container.slider__main', {
+  pagination: {
+    el: '.swiper-pagination.slider__pagination',
+    dynamicBullets: true,
+  },
+  loop: true,
+  speed: 500,
+  autoplay: 500,
+});
+
+// Now you can use all slider methods like
+cboxSlider.slideNext();
+mainSwiper.slideNext();
