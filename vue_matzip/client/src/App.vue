f66413c2500303c943dc1058ea87a6546ec5abdb
jonsoku2 <whdtjr2792@gmail.com>
2020-05-16 15:51:21 +0900

    [vue-matzip-dev] 프로젝트 초기화

--- vue_matzip/client/src/App.vue
+++ vue_matzip/client/src/App.vue
@@ -1,55 +1,13 @@
 <template>
   <div>
-    <Header />
-    <transition name="slide-fade" mode="out-in">
-      <router-view />
-    </transition>
+    App
   </div>
 </template>
 
 <script>
-import { mapActions, mapGetters } from "vuex";
-import Header from "./components/global/Header";
-
 export default {
   name: "App",
-  components: {
-    Header,
-  },
-  methods: {
-    ...mapActions(["getMe"]),
-  },
-  computed: {
-    ...mapGetters(["isLoggedIn"]),
-  },
-  created() {
-    if (window.sessionStorage.getItem("token")) {
-      this.getMe();
-    }
-  },
 };
 </script>
 
-<style lang="scss">
-@import "./assets/scss/main.scss";
-.auth {
-  &__wrapper {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-  }
-}
-.slide-fade-enter {
-  transform: translateX(10px);
-  opacity: 0;
-}
-.slide-fade-enter-active,
-.slide-fade-leave-active {
-  transition: all 0.2s ease;
-}
-.slide-fade-leave-to {
-  transform: translateX(10px);
-  opacity: 0;
-}
-</style>
+<style lang="scss" scoped></style>
